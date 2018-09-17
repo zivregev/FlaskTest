3d061f831aef819683d4d230b524822497340a09
z_regev <zivregevt@gmail.com>
2018-09-17 15:09:05 +0300

    f

--- WebFixer.py
+++ WebFixer.py
@@ -10,11 +10,13 @@ def form():
         <html>
             <body>
                 <h1>HUJI medicine schedule fixer</h1>
-                First, get your schedule file from the rishum-net. This is an *.ics file. You can find it when rishum is active, as described in the following images:
+                First, get your schedule file from the rishum-net. This is an *.ics file. You can find it when rishum is active:
                 <br>
                 <img src="static/cal_tut_1.png">
                 <br>
                 <img src="static/cal_tut_2.png">
+                <br>
+                Just save the file, don't open it (Windows will try to open it automatically using Outlook).
                 <br>
                 <form action="/fixSchedule" method="post" enctype="multipart/form-data">
                     Now, upload the *.ics file here: <input type="file" name="data_file" />
@@ -22,6 +24,13 @@ def form():
                     Enter the school year (E.g., for 2018-19, write 2019) <input type="string" name="year" />
                     <input type="submit" value="Fix"/>
                 </form>
+                <br>
+                Now, import the new "fixed_schedule.ics" file to your favorite calendar. 
+                For example, if you're using Google Calendar (I suggest creating a totally new calendar to import into, in case something breaks):
+                <br>
+                <img src="static/cal_tut_3.png">
+                <br>
+                <img src="static/cal_tut_4.png">
             </body>
         </html>
     """
